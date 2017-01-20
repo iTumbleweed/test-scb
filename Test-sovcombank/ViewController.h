13c957eb15f9d20926b0e769652a5426a1355977
Ruslan <ruslan@ruslans-macbook-pro.local>
2017-01-20 14:16:48 +0300

    Initial Commit

---
+++ Test-sovcombank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test-sovcombank
+//
+//  Created by Ruslan on 20/01/2017.
+//  Copyright © 2017 Ruslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
